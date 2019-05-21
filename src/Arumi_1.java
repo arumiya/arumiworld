4deaf87089150c2012e3e1be45f820373b8f0f32
Arumi <Arumi@218.38.137.27>
2019-05-22 00:06:50 +0900

    initial commit

---
+++ src/Arumi_1.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+
+public class Arumi_1 {
+	public static void sub(String name) {
+		System.out.println("This is" + name);
+	}
+	
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.print("HelloWorld");
+		System.out.println("HelloWorld");
+		int a;
+		a = scan.nextInt();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		sub("Sangjun");
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		sub("Arum");
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println(a);
+		
+	}
+
+	
+}
+
